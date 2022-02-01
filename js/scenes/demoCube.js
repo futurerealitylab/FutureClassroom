127b8bc228b991d99e315ecf64dce50ec36b73a1
zhenqixu <susanna@brandeis.edu>
2022-01-31 21:20:48 -0500

    Update demoCube.js

--- js/scenes/demoCube.js
+++ js/scenes/demoCube.js
@@ -4,13 +4,15 @@ let DemoCube = function() {
       this.start = true;
       this.name = "earth";
       this.model = model;
-      this.model.move(0,1.5,0).scale(0.2);
-      this.earth = this.model.add('sphere').scale(100).color(1,1,1).texture('media/textures/earth.png');
+      this.model.move(0,1.5,0).scale(0.5);
+      this.earth = this.model.add('sphere').color(1,1,1).texture('media/textures/earth.png');
+      this.light = this.model.add('donut').color(255,255,0).texture('media/textures/brick.png');
    }
 
    this.display = () => {
       this.model.animate(() => {
          this.earth.identity().turnY(0.2 * this.model.time);
+         this.light.identity().turnX(0.5 * this.model.time);
       });
    }
 }
